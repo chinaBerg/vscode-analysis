a689769c4730d8f1648b5f9c7e00751e71fad957
刘顺利 <1563940776@qq.com>
2022-12-05 21:54:06 +0800

    Analyze postinstall.js

--- build/npm/postinstall.js
+++ build/npm/postinstall.js
@@ -9,6 +9,7 @@ const { setupBuildYarnrc } = require('./setupBuildYarnrc');
 const yarn = process.platform === 'win32' ? 'yarn.cmd' : 'yarn';
 
 /**
+ * 在指定目录下运行yarn install命令安装依赖包
  * @param {string} location
  * @param {*} [opts]
  */
@@ -21,6 +22,8 @@ function yarnInstall(location, opts) {
 	const argv = JSON.parse(raw);
 	const original = argv.original || [];
 	const args = original.filter(arg => arg === '--ignore-optional' || arg === '--frozen-lockfile' || arg === '--check-files');
+	// 忽略package.json中engines字段的检查
+	// 即忽略依赖资源检查
 	if (opts.ignoreEngines) {
 		args.push('--ignore-engines');
 		delete opts.ignoreEngines;
@@ -28,13 +31,17 @@ function yarnInstall(location, opts) {
 
 	console.log(`Installing dependencies in ${location}...`);
 	console.log(`$ yarn ${args.join(' ')}`);
+
+	// 安装依赖
 	const result = cp.spawnSync(yarn, args, opts);
 
+	// 安装失败退出
 	if (result.error || result.status !== 0) {
 		process.exit(1);
 	}
 }
 
+// 遍历./dir.js文件内列出的所有文件路径
 for (let dir of dirs) {
 
 	if (dir === '') {
@@ -47,8 +54,11 @@ for (let dir of dirs) {
 		continue;
 	}
 
+	// 处理build文件夹
 	if (dir === 'build') {
+		// 生成build/.yarnrc文件
 		setupBuildYarnrc();
+		// 安装build/目录下所有的依赖包
 		yarnInstall('build');
 		continue;
 	}
@@ -67,11 +77,23 @@ for (let dir of dirs) {
 
 		opts = { env };
 	} else if (/^extensions\//.test(dir)) {
+		// extensions/开头的路径，忽略内部的engines字段检查，不检查依赖资源
 		opts = { ignoreEngines: true };
 	}
 
+	// 安装对应文件夹下面的依赖包
 	yarnInstall(dir, opts);
 }
 
+/**
+ * 通过子进程运行 git config pull.rebase merges 命令, 等同于设置 git config pull --rebase --rebase-merges
+ * pull操作本身是就是 git fetch + git merge或者git fetch + git rebase的组合
+ * @see https://git-scm.com/docs/git-pull#Documentation/git-pull.txt---rebasefalsetruemergesinteractive
+ */
 cp.execSync('git config pull.rebase merges');
+
+/**
+ * 通过子进程运行 git config blame.ignoreRevsFile .git-blame-ignore 命令, 等同于设置 git config blame --ignore-revs-file .git-blame-ignore
+ * @see https://git-scm.com/docs/git-blame#Documentation/git-blame.txt---ignore-revs-fileltfilegt
+ */
 cp.execSync('git config blame.ignoreRevsFile .git-blame-ignore');
--- build/npm/setupBuildYarnrc.js
+++ build/npm/setupBuildYarnrc.js
@@ -9,17 +9,22 @@ const fs = require('fs');
 // make sure we install the deps of build for the system installed
 // node, since that is the driver of gulp
 function setupBuildYarnrc() {
+	// 获取要写入的.yarnrc文件路径
+	// 等同于../../.yarnrc，实际是build/.yarnrc
 	const yarnrcPath = path.join(path.dirname(__dirname), '.yarnrc');
+	// 待写入的.yarnrc文件内容
 	const yarnrc = `disturl "https://nodejs.org/download/release"
 target "${process.versions.node}"
 runtime "node"
 arch "${process.arch}"`;
 
+	// 写入到.yarnrc
 	fs.writeFileSync(yarnrcPath, yarnrc, 'utf8');
 }
 
 exports.setupBuildYarnrc = setupBuildYarnrc;
 
+// 只有直接运行该脚本时才执行
 if (require.main === module) {
 	setupBuildYarnrc();
 }
